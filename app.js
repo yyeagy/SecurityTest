9678db9b60921a1d1cfd2b6320f4a87eaca359a8
yyeag <yyeagy@gmail.com>
2021-01-02 12:53:42 -0500

    Init commit

---
+++ app.js
@@ -0,0 +1,66 @@
+//jshint esversion:6
+require('dotenv').config()
+const express = require("express");
+const bodyParser = require("body-parser"); 
+const _ = require('lodash');
+const mongoose = require('mongoose');
+var encrypt = require('mongoose-encryption');
+
+const app = express();
+app.set('view engine', 'ejs');
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(express.static("public"));
+ 
+mongoose.connect(process.env.DB_URI,{useNewUrlParser: true, useUnifiedTopology:true});
+
+const usersSchema = new mongoose.Schema({
+    email: String,
+    password: String
+  }); 
+
+ 
+usersSchema.plugin(encrypt, { secret: process.env.SECRECT, encryptedFields: ['password']  });
+  
+const User = mongoose.model("User", usersSchema);
+
+app.get("/", (req, res) => {
+    res.render("home");
+});
+
+app.get("/login", (req, res) => {
+    res.render("login");
+});
+
+app.post("/login", (req, res) => { 
+    User.findOne({email: req.body.username}, (err, result) => {
+        if(err)
+            return res.send(err);
+      
+        if(result && result.password === req.body.password){
+            res.render("secrets");
+        }
+        else{
+            res.send("log in failed");
+        }
+    }); 
+});
+
+app.get("/register", (req, res) => {
+    res.render("register");
+}); 
+
+app.post("/register", (req, res) => {
+    const newUser = new User({
+        email: req.body.username,
+        password: req.body.password
+    });
+    newUser.save((err, result) => {
+        if(err)
+           return res.send(err);
+        res.render("secrets");
+    });
+});
+
+app.listen(process.env.PORT||3000, function() {
+    //console.log("Server started on port 3000");
+  });
